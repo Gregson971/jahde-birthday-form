4af87432f2b07c4934e4270a982fc724a56530df
Grégory Lama <gregory.lama@gmail.com>
2024-02-15 21:36:42 +0100

    change db url

--- app.py
+++ app.py
@@ -18,7 +18,6 @@ db_password = os.getenv("DB_PASSWORD")
 db_host = os.getenv("DB_HOST")
 db_port = os.getenv("DB_PORT")
 db_name = os.getenv("DB_NAME")
-env = os.getenv("ENV")
 
 db_url = f"postgresql+psycopg2://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
 
