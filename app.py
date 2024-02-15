f59ce055347f0cc0ad71756c8cd2e6d720f51e0f
Grégory Lama <gregory.lama@gmail.com>
2024-02-15 21:26:17 +0100

    change db url

--- app.py
+++ app.py
@@ -18,8 +18,12 @@ db_password = os.getenv("DB_PASSWORD")
 db_host = os.getenv("DB_HOST")
 db_port = os.getenv("DB_PORT")
 db_name = os.getenv("DB_NAME")
+env = os.getenv("ENV")
 
-db_url = f"postgresql+psycopg2://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
+if env == "production":
+    db_url = os.getenv("HEROKU_POSTGRESQL_BRONZE_URL")
+else:
+    db_url = f"postgresql+psycopg2://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
 
 # Create a new Flask app
 app = Flask(__name__)
